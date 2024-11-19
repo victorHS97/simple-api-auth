638a0d9b44ddee7a98a2af81be3deb65b81ab527
Victor Souza <victor.souza37@outlook.com>
2024-11-18 22:49:14 -0300

    finally login

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, jsonify
 from database import db
 from models.user import User
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 
 # LIBS
-login_manager = LoginManager
+login_manager = LoginManager()
 app = Flask(__name__)
 
 # CONFIG FLASK
@@ -13,15 +13,26 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 
 db.init_app(app)
 login_manager.init_app(app)
+# informa qual rota é a de login para o gerenciador Login Manager
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader  # carrega uma sessão com informações do usuario
+def load_user(user_id):
+    return User.query.get(user_id)
 
 
 @app.route('/login', methods=['POST'])
 def login():
-    data = request.json()
+    data = request.json
     username = data.get('username')
     password = data.get('password')
-    if username and password:
-        # login
+    # abaixo é feita uma busca no banco o usuario pelo username
+    user = User.query.filter_by(username=username).first()
+    if user and password == user.password:
+        login_user(user)
+        return jsonify({"Message": "Autenticação feita com sucesso"})
+
     return jsonify({"Message": "Credenciais invalidas"}), 400
 
 
